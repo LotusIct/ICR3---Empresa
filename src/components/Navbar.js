c58033385ab001e17d45c21073a5a8f806779418
Michaela Galbiati <michaela.galbiati@gmail.com>
2025-07-01 09:03:47 -0300

    Atualiza seção menu e home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,55 +11,58 @@ const Navbar = () => {
   const location = useLocation();
 
   const getActive = (path) => (location.pathname === path ? 'active' : '');
-
+  const handleLinkClick = () => {
+      setMenuOpen(false);
+      setDropdownOpen(false);
+    };
   return (
     <header className="navbar">
       <div className="navbar-left">
         <div className="logo">
           <img src={logo} alt="Logo ICR3 Científica" />
         </div>
-        <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
-          <ul className="nav-links">
-            <li>
-              <Link to="/" className={getActive('/')}>Home</Link>
-            </li>
-            <li>
-              <Link to="/sobre" className={getActive('/sobre')}>Sobre Nós</Link>
-            </li>
-            <li>
-              <Link to="/serviços" className={getActive('/serviços')}>Serviços</Link>
-            </li>
-
-            {/* Dropdown de Marcas por Categoria */}
-            <li
-              className="dropdown"
-              onMouseEnter={() => setDropdownOpen(true)}
-              onMouseLeave={() => setDropdownOpen(false)}
-            >
-              <span className={`nav-link ${getActive('/marcas')}`}>Marcas</span>
-              {isDropdownOpen && (
-                <div className="megamenu">
-                  {marcas.map((categoria, idx) => (
-                    <div className="megamenu-column" key={idx}>
-                      <h4>{categoria.categoria}</h4>
-                      <ul>
-                        {categoria.marcas.map((marca) => (
-                          <li key={marca.id}>
-                            <Link to={`/marcas/${marca.id}`}>{marca.nome}</Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </li>
-
-            <li>
-              <Link to="/fale-conosco" className={getActive('/fale-conosco')}>Fale Conosco</Link>
-            </li>
-          </ul>
-        </nav>
+       <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+        
+      <ul className="nav-links">
+    <li>
+      <Link to="/" className={getActive('/')} onClick={handleLinkClick}>Home</Link>
+    </li>
+    <li>
+      <Link to="/sobre" className={getActive('/sobre')} onClick={handleLinkClick}>Sobre Nós</Link>
+    </li>
+    <li>
+      <Link to="/serviços" className={getActive('/serviços')} onClick={handleLinkClick}>Serviços</Link>
+    </li>
+    <li
+      className="dropdown"
+      onMouseEnter={() => setDropdownOpen(true)}
+      onMouseLeave={() => setDropdownOpen(false)}
+    >
+      <span className={`nav-link ${getActive('/marcas')}`}>Marcas</span>
+      {isDropdownOpen && (
+        <div className="megamenu">
+          {marcas.map((categoria, idx) => (
+            <div className="megamenu-column" key={idx}>
+              <h4>{categoria.categoria}</h4>
+              <ul>
+                {categoria.marcas.map((marca) => (
+                  <li key={marca.id}>
+                    <Link to={`/marcas/${marca.id}`} onClick={handleLinkClick}>
+                      {marca.nome}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </li>
+    <li>
+      <Link to="/fale-conosco" className={getActive('/fale-conosco')} onClick={handleLinkClick}>Fale Conosco</Link>
+    </li>
+  </ul>
+      </nav>
       </div>
 
       <div className="social-bar">
