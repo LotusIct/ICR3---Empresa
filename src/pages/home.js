a50954c2ee414d08b12e45dbcdad26d48b42ab23
Michaela Galbiati <michaela.galbiati@gmail.com>
2025-08-19 09:50:04 -0300

    Ajustes imagem

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/home.css';
-import videoHome from '../assets/home.mp4'; 
+import videoHome from '../assets/icr3.MP4'; 
 import { useNavigate } from 'react-router-dom';
 
 import logo1 from '../assets/logos/Logo aro .png';
